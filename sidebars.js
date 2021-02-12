4ba29cf1b52ca42006e16d7d619854d7fbb481fb
steph222 <stephdellbre@gmail.com>
2021-02-12 14:02:33 -0600

    fix(frontend): Sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,59 +1,59 @@
 module.exports = {
     docs: {
         "Guías generales": [
-            "cultura-ingenieril",
             "pautas-para-codigo-abierto",
+            "cultura-ingenieril",
             "metodologias-agiles",
             "programacion-parejas-grupos",
-            "devops",
             "herramientas-para-desarrolladores",
             "proyectos-de-codigo-abierto",
             "boilerplate",
             "material-ui", 
             "recursos-visuales",
             "redes-sociales",
-            "plantilla-readme",
-            "kubernetes"
+            "devops"           
         ],
         "Blockchain EOSIO": [
             "aprender-eosio/protocolo-eosio",
             "aprender-eosio/mecanismo-de-consenso",
             "aprender-eosio/uso-de-recursos",
+            "aprender-eosio/componentes-eosio",
             "aprender-eosio/cuentas-permisos",
             "aprender-eosio/funciones-importantes",
             "aprender-eosio/llaves-privadas",
             "aprender-eosio/desarrollo-de-dapps",
             "aprender-eosio/cadena-de-bloques-eos",
             "aprender-eosio/contratos-inteligentes",
-            "aprender-eosio/contratos-ricardianos",
-            "aprender-eosio/componentes-eosio"
+            "aprender-eosio/contratos-ricardianos"
+            
         ],
         "Tutoriales": [
-            "tutoriales/instalacion-de-nodos",
-            "tutoriales/contrato-hola-mundo",
-            "tutoriales/cpp-moderno",
-            "tutoriales/tutorial-flujo-trabajo-git",
             "tutoriales/guia-markdown",
+            "tutoriales/tutorial-flujo-trabajo-git",
             "tutoriales/guia-github" ,
             "tutoriales/llaves-github",
             "tutoriales/tutorial-react",
             'tutoriales/graphql',
-            "tutoriales/docker"
-
+            "tutoriales/docker",
+            "tutoriales/kubernetes",
+            "tutoriales/instalacion-de-nodos",
+            "tutoriales/contrato-hola-mundo",
+            "tutoriales/cpp-moderno"
         ],
         "Herramientas": [
             "herramientas/glosario",
             "herramientas/linea-de-comando",
             "herramientas/opciones-configuracion-nodeos",
             "herramientas/explorador-de-bloques",
-            "herramientas/librerias-sdk",
+            "herramientas/librerias-sdk",      
+            "herramientas/plantilla-readme",
             "herramientas/preguntas-frecuentes"
         ],
         "Comunidad": [
+            "comunidad/redes-eosio" ,
             "comunidad/canales-de-telegram",
             "comunidad/publicaciones-blog",
             "comunidad/podcasts-eos",
-            "comunidad/redes-eosio" ,
             "comunidad/enlaces-utiles"
         ]
 
